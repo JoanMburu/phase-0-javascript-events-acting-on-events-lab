62586024f35731059df4ddd46a27eef9027b37a2
JoanMburu <joan.wangui@student.moringaschool.com>
2024-04-20 23:40:05 +0300

    acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById ('dodger');
+
+// Moving dodger to the left
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers,10);
+
+    if (left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+// Adding an event listener 
+document.addEventListener('keydown', function(e){
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+});
+
+// Moving dodger to the right
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace ('px', '');
+    const left = parseInt (leftNumbers,10);
+
+    const rightEdge = window.innerWidth - dodger.offsetWidth;
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+// Adding an event listener
+document.addEventListener ('keydown', function (e){
+    if (e.key === 'ArrowRight'){
+    moveDodgerRight();
+}
+});
